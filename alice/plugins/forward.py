487659e66a73e607d9292446bde7cf0e6ed19d5d
wulan17 <wulan17@nusantararom.org>
2022-11-27 21:42:54 +0700

    Alice: Set parse mode to HTML
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- alice/plugins/forward.py
+++ alice/plugins/forward.py
@@ -3,7 +3,7 @@ import os
 from alice.db import setting as sql
 from alice.alice import Alice
 from alice.utils.string import build_keyboard, parse_button
-from pyrogram import filters
+from pyrogram import enums, filters
 from pyrogram.types import InlineKeyboardMarkup
 
 @Alice.on_message(filters.group & filters.command("settopics"))
@@ -41,7 +41,7 @@ async def forward(c,m):
 			else:
 				button = None
 			os.remove(os.path.join(os.getcwd(), 'temp2.txt'))
-			return await c.send_message(chat_id, text, reply_to_message_id=thread_id, reply_markup=button)
+			return await c.send_message(chat_id, text, reply_to_message_id=thread_id, reply_markup=button, parse_mode=enums.ParseMode.HTML)
 		if m.caption:
 			with open('temp2.txt', 'w') as f:
 				f.write(m.caption.html)
@@ -57,15 +57,15 @@ async def forward(c,m):
 			button = None
 		file = await m.download()
 		if m.photo:
-			await c.send_photo(chat_id=chat_id, photo=file, caption=caption, reply_to_message_id=thread_id, reply_markup=button)
+			await c.send_photo(chat_id=chat_id, photo=file, caption=caption, reply_to_message_id=thread_id, reply_markup=button, parse_mode=enums.ParseMode.HTML)
 		elif m.document:
-			await c.send_document(chat_id=chat_id, document=file, caption=caption, reply_to_message_id=thread_id, reply_markup=button)
+			await c.send_document(chat_id=chat_id, document=file, caption=caption, reply_to_message_id=thread_id, reply_markup=button, parse_mode=enums.ParseMode.HTML)
 		elif m.video:
-			await c.send_video(chat_id=chat_id, video=file, caption=caption, reply_to_message_id=thread_id, reply_markup=button)
+			await c.send_video(chat_id=chat_id, video=file, caption=caption, reply_to_message_id=thread_id, reply_markup=button, parse_mode=enums.ParseMode.HTML)
 		elif m.animation:
-			await c.send_animation(chat_id=chat_id, animation=file, caption=caption, reply_to_message_id=thread_id, reply_markup=button)
+			await c.send_animation(chat_id=chat_id, animation=file, caption=caption, reply_to_message_id=thread_id, reply_markup=button, parse_mode=enums.ParseMode.HTML)
 		elif m.audio:
-			await c.send_audio(chat_id=chat_id, audio=file, caption=caption, reply_to_message_id=thread_id, reply_markup=button)
+			await c.send_audio(chat_id=chat_id, audio=file, caption=caption, reply_to_message_id=thread_id, reply_markup=button, parse_mode=enums.ParseMode.HTML)
 		elif m.sticker:
 			await c.send_sticker(chat_id=chat_id, sticker=m.sticker.file_id, reply_to_message_id=thread_id)
 		os.remove(file)
