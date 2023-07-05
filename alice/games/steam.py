10aef0dd0e8006d76c28fde158f2f2a69e9e4844
wulan17 <wulan17@nusantararom.org>
2023-07-05 18:49:32 +0700

    Alice: games: steam: Move to httpx
    Signed-off-by: wulan17 <wulan17@nusantararom.org>

--- alice/games/steam.py
+++ alice/games/steam.py
@@ -1,42 +1,43 @@
+import httpx
 import re
 import os
 from pathlib import Path
 
-import bs4
-import requests
 from alice import GAME_CHAT
 from bs4 import BeautifulSoup
+from pyrogram import utils
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 UA = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0"
 STEAM_URL = "https://store.steampowered.com/search/?maxprice=free&specials=1"
 
 async def get_free_steam_games(client):
+	requests = httpx.AsyncClient(http2=True)
 	db = client.db['freegames']
-	request = requests.get(STEAM_URL)
-	soup = BeautifulSoup(request.text, "html.parser")
+	request = await requests.get(STEAM_URL, headers={"User-Agent": UA}, timeout=30)
+	soup = await utils.run_sync(BeautifulSoup, request.text, "html.parser")
 
-	games = soup.find_all("a", class_="search_result_row")
+	games = await utils.run_sync(soup.find_all, "a", class_="search_result_row")
 	for game in games:
-		game_name: str = await get_game_name(game)
+		game_name: str = get_game_name(game)
 		all_games = (await db.find_one({'name': 'steam'}))['game_name']
 		if game_name in all_games:
 			continue
-		game_url: str = await get_game_url(game)
-		image_url: str = await get_game_image(game)
-		request2 = requests.get(game_url)
-		soup2 = BeautifulSoup(request2.text, "html.parser")
-		original_price = soup2.select_one(".discount_original_price").get_text()
-		discount_price = soup2.select_one(".discount_final_price").get_text()
-		text = soup2.find_all('div',{'class': "game_area_description"})
+		game_url: str = get_game_url(game)
+		image_url: str = get_game_image(game)
+		request2 = await requests.get(game_url, headers={"User-Agent": UA}, timeout=30)
+		soup2 = await utils.run_sync(BeautifulSoup, request2.text, "html.parser")
+		original_price = (await utils.run_sync(soup2.select_one, ".discount_original_price")).get_text()
+		discount_price = (await utils.run_sync(soup2.select_one, ".discount_final_price")).get_text()
+		text = await utils.run_sync(soup2.find_all, 'div',{'class': "game_area_description"})
 		desc = 'No description available'
 		for t in text:
 			f = re.search(r'(About This)(\s){0,}(Game|Content)(\s){0,}([A-Za-z0-9\.\!\?\n].*\n.*)', t.get_text('\n'))
 			if f:
 				desc = f.group(5)
-		text = soup2.select_one(".game_purchase_discount_quantity").get_text()
+		text = (await utils.run_sync(soup2.select_one, ".game_purchase_discount_quantity")).get_text()
 		exp = re.search(r'(before\s)([A-Za-z0-9\@\:\s]{0,})', text).group(2)
-		text = soup2.select_one(".game_area_purchase").get_text()
+		text = (await utils.run_sync(soup2.select_one, ".game_area_purchase")).get_text()
 		if re.search("Downloadable Content", text):
 			offer_type = "DLC"
 			hastag = "#freedlc #dlc"
@@ -56,8 +57,9 @@ async def get_free_steam_games(client):
 		)
 		await client.send_photo(chat_id=GAME_CHAT, photo=image_url, caption=text, reply_markup=button)
 		await db.update_one({'name': 'steam'},{"$push": {'game_name': game_name}})
+		await requests.aclose()
 
-async def get_game_image(game):
+def get_game_image(game):
 	"""
 	Get the game ID and create image URL.
 	Args:
@@ -70,7 +72,7 @@ async def get_game_image(game):
 	return image_url
 
 
-async def get_game_url(game):
+def get_game_url(game):
 	"""
 	Get the game url.
 	Args:
@@ -82,7 +84,7 @@ async def get_game_url(game):
 	return game_url
 
 
-async def get_game_name(game):
+def get_game_name(game):
 	"""
 	Get the game name.
 	Args:
@@ -90,7 +92,6 @@ async def get_game_name(game):
 	Returns:
 		The game name.
 	"""
-	game_name_class: bs4.element.Tag = game.find("span", class_="title")
+	game_name_class = game.find("span", class_="title")
 	game_name = game_name_class.text
 	return game_name
-
